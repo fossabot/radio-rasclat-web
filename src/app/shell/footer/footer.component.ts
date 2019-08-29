052def9a8f7f0e64fb33ff59bdf9420db5121ece
Domenik Töfflinger <domenikt96@yahoo.de>
2019-08-29 02:55:13 +0200

    Re-enables _audioContext.

--- src/app/shell/footer/footer.component.ts
+++ src/app/shell/footer/footer.component.ts
@@ -9,30 +9,29 @@ import { AudioContext } from 'angular-audio-context';
   providers: [DataService]
 })
 export class FooterComponent implements OnInit {
-  // message: string;
+  message: string;
+
+  constructor(private data: DataService, private _audioContext: AudioContext) {}
   //
-  // constructor(private data: DataService, private _audioContext: AudioContext) {}
-  //
-  // public async beep(): Promise<void> {
-  //   if (this._audioContext.state === 'suspended') {
-  //     this.message = 'hi';
-  //     await this._audioContext.resume();
-  //   }
-  //
-  //   if (this._audioContext.state === 'running') {
-  //     this.message = 'play';
-  //   }
-  //
-  //   const oscillatorNode = this._audioContext.createOscillator();
-  //
-  //   oscillatorNode.onended = () => oscillatorNode.disconnect();
-  //   oscillatorNode.connect(this._audioContext.destination);
-  //
-  //   oscillatorNode.start();
-  //   oscillatorNode.stop(this._audioContext.currentTime + 0.5);
-  // }
-  //
+  public async beep(): Promise<void> {
+    if (this._audioContext.state === 'suspended') {
+      this.message = 'hi';
+      await this._audioContext.resume();
+    }
+
+    if (this._audioContext.state === 'running') {
+      this.message = 'play';
+    }
+
+    const oscillatorNode = this._audioContext.createOscillator();
+
+    oscillatorNode.onended = () => oscillatorNode.disconnect();
+    oscillatorNode.connect(this._audioContext.destination);
+    oscillatorNode.start();
+    oscillatorNode.stop(this._audioContext.currentTime + 0.5);
+  }
+
   ngOnInit() {
-    // this.data.currentMessage.subscribe(message => (this.message = message));
+    this.data.currentMessage.subscribe(message => (this.message = message));
   }
 }
